47fc67c2976e67fb4d3e372283746d55ab231b14
cwklinus22015 <student+22015@codingwithkids.com>
2019-08-19 15:25:31 -0700

    new awesome version

--- whatever.py
+++ whatever.py
@@ -5,6 +5,8 @@ def main():
     print("{} n'est pas grand mais il est vaillant".format(name))
     print("tu es vraiment tres beau {}".format(name))
     print("as expected, great job {}".format(name))
+    print("wake the heck up samourai {}, we've got a city to burn".format(name))
+
 
 
 
