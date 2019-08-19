a269445b9db11a8da1fe35e9b3b96f4907809999
cwklinus22015 <student+22015@codingwithkids.com>
2019-08-19 15:22:29 -0700

    changed to mamen

--- whatever.py
+++ whatever.py
@@ -1,5 +1,15 @@
 def main():
-    print("mamen")
+    name = input("What is your name")
+    print("how are you {}".format(name))
+    print("du sale mamene {}".format(name))
+    print("{} n'est pas grand mais il est vaillant".format(name))
+    print("tu es vraiment tres beau {}".format(name))
+    print("as expected, great job {}".format(name))
+
+
+
+
+    # print("mamene kirikou")
 
 
 if __name__ == "__main__":
